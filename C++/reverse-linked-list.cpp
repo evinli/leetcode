910b3d5506a2a4f59c4933bebaa49e88e88cc069
Evin Li <evinywli@gmail.com>
2022-09-30 01:04:00 -0700

    new solns

--- C++/reverse-linked-list.cpp
+++ C++/reverse-linked-list.cpp
@@ -1,3 +1,5 @@
+//https://medium.com/outco/reversing-a-linked-list-easy-as-1-2-3-560fbffe2088
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -14,14 +16,15 @@ public:
         ListNode* previous = nullptr;
         ListNode* curr = head;
         ListNode* next = nullptr;
-                
+
         while (curr != nullptr) {
             next = curr->next; // move next pointer to next node
             curr->next = previous; // set arrow to point from current node to previous node
             previous = curr; // move previous pointer to current node
             curr = next; // move current pointer to next node
         }
-        
+
         return previous; // return head of reversed list
     }
 };
+
