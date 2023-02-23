2d325b6dd939753bef281c73b3712b4fbb99a082
Evin Li <evinywli@gmail.com>
2023-02-22 23:32:49 -0800

    new leetcode

---
+++ Java/climbing-stairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1 || n == 2) return n;
+        int prev1 = 1;
+        int prev2 = 2;
+        int ans = 0;
+
+        for (int i = 3; i <= n; i++) {
+            ans = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = ans;
+        }
+        return ans;
+    }
+}
+
+// n = 3
+
+// 111
+// 12
+// 21
+
+// n = 4
+// 1111
+// 22
+// 121
+// 112
+// 211
+
